a2675c0b0449b41f1a3ed9f63e547ef78318f110
Ludvig Sterky <Ludvig.Sterky@elev.ga.ntig.se>
2024-11-08 13:21:52 +0100

    fef

---
+++ näsan övning 2/näsan övning 2/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+string namn = "";
+
+System.Console.WriteLine("Skriv in ditt användarnamn");
+namn = Console.ReadLine();
+if(namn.ToLower() == "peter"){
+System.Console.WriteLine("välkommen till Sverige");}
+
+while(namn.ToLower() != "peter"){
+    System.Console.WriteLine("Fel användarnamn, testa igen!");
+    namn = Console.ReadLine();
+    if(namn.ToLower() == "peter"){
+System.Console.WriteLine("välkommen till Sverige");}
+}
+
+Console.ReadLine();
