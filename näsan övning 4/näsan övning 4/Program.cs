5a02cfdacfa582371ee168f19806fffcb32385a1
Ludvig Sterky <Ludvig.Sterky@elev.ga.ntig.se>
2024-11-11 09:37:20 +0100

    klar

---
+++ näsan övning 4/näsan övning 4/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+string lösenord = "";
+
+System.Console.WriteLine("Du ska logga in på din dator och du måste ange ett lösenord.");
+System.Console.WriteLine("Vad är lösenordet?");
+lösenord = Console.ReadLine();
+if(lösenord == "12345"){
+    System.Console.WriteLine("Välkommen");
+}
+while(lösenord != "12345"){
+    System.Console.WriteLine("Skriv in lösenord igen!");
+    lösenord = Console.ReadLine();
+    if(lösenord == "12345"){
+        System.Console.WriteLine("Välkommen!");
+    }
+}
+Console.ReadLine();
+
