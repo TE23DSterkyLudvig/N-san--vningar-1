2953b7019c7d9d42a056253baa578ea44dcf6a9b
Ludvig Sterky <Ludvig.Sterky@elev.ga.ntig.se>
2024-11-11 09:47:56 +0100

    klar

---
+++ näsan övning 5/näsan övning 5/Program.cs
@@ -0,0 +1,17 @@
+﻿int tal;
+string talbok = "";                                                        //talbokstav
+
+
+for (int i = 0; i < 5; i++)
+{
+  System.Console.WriteLine("Ange ett tal");  
+  talbok = Console.ReadLine();
+  int.TryParse(talbok, out tal);
+  if (tal > 5){
+    System.Console.WriteLine("Högre än 5");
+  }
+  else{
+    System.Console.WriteLine("mindre eller lika med 5.");
+  }
+}
+Console.ReadLine();
