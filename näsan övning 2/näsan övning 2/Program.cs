ffbc985eb949edcef9dd0d5f658ba368227b14a6
Ludvig Sterky <Ludvig.Sterky@elev.ga.ntig.se>
2024-11-11 09:19:24 +0100

    klar

--- näsan övning 2/näsan övning 2/Program.cs
+++ näsan övning 2/näsan övning 2/Program.cs
@@ -1,17 +1,31 @@
 ﻿
 
 string namn = "";
+string lösenord = "";
 
 System.Console.WriteLine("Skriv in ditt användarnamn");
 namn = Console.ReadLine();
-if(namn.ToLower() == "peter"){
-System.Console.WriteLine("välkommen till Sverige");}
+if(namn.ToLower() == "kalleanka"){
+System.Console.WriteLine("Rätt användarnamn, Vad är lösenordet?");
+lösenord = Console.ReadLine();
+if(lösenord.ToLower() == "12345"){
+    System.Console.WriteLine("välkommen till Sverige");
+    }
+        while(lösenord!= "12345" ){
+        System.Console.WriteLine("skriv in igen");
+        lösenord = Console.ReadLine();}
+        if(lösenord == "12345"){
+            System.Console.WriteLine("Välkommen");
+        }
+    }
 
-while(namn.ToLower() != "peter"){
+while(namn.ToLower() != "kalleanka"){
     System.Console.WriteLine("Fel användarnamn, testa igen!");
     namn = Console.ReadLine();
-    if(namn.ToLower() == "peter"){
-System.Console.WriteLine("välkommen till Sverige");}
 }
+    
+
+
+
 
 Console.ReadLine();
