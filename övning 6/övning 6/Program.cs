1852dbd64ae5f6f3af9a7403f7d115b1a1f5b6c6
Ludvig Sterky <Ludvig.Sterky@elev.ga.ntig.se>
2024-11-15 10:28:03 +0100

    klar

---
+++ övning 6/övning 6/Program.cs
@@ -0,0 +1,19 @@
+﻿string text = "";
+int textnum;
+
+
+while(true){
+    System.Console.WriteLine("Ange ett nummer!");
+text = Console.ReadLine();
+    if(int.TryParse(text,out textnum)){
+        break;
+    }
+    else{
+        System.Console.WriteLine("Skriv ett nummer!");
+        Thread.Sleep(800);
+    }
+}
+while(int.TryParse(text,out textnum)){
+    System.Console.WriteLine("Minnesota");
+    Thread.Sleep(4000);
+}
