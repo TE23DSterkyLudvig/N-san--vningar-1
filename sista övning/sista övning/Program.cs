c45a4bcdbfef64149a0e1342361d86a2675820c1
Ludvig Sterky <Ludvig.Sterky@elev.ga.ntig.se>
2024-11-18 08:58:52 +0100

    klar

--- sista övning/sista övning/Program.cs
+++ sista övning/sista övning/Program.cs
@@ -3,35 +3,41 @@ string gissning = "";
 int gissningnum;
 
 
-while(true){
-    rätttal = Random.Shared.Next(4,20);
+while (true)
+{
+    rätttal = Random.Shared.Next(4, 20);
     System.Console.WriteLine("Du ska gissa ett tal som är mellan 20 och 4. Börja gissa!");
     gissning = Console.ReadLine();
-    if(int.TryParse(gissning, out gissningnum)){
-        int.TryParse(gissning, out gissningnum);
-       // while(gissningnum > 20 && gissningnum < 4){
-         //   System.Console.WriteLine("Skriv ett tal mellan 20 och 4!");
-         //   gissning = Console.ReadLine();
-         //   int.TryParse(gissning, out gissningnum);
-        System.Console.WriteLine($"Du gissade på {gissningnum}!");
-        break;
-    }
-    else{
+    // int.TryParse(gissning, out gissningnum);
+    if (int.TryParse(gissning, out gissningnum) == false)
+    {
         System.Console.WriteLine("Skriv en siffra!");
         Thread.Sleep(800);
     }
+    else if (gissningnum < 4 || gissningnum > 20)
+    {
+        System.Console.WriteLine("Skriv en siffra mellan 20 och 4");
+        gissning = Console.ReadLine();
+    }
+    else
+    {
+        System.Console.WriteLine($"Du gissade på {gissningnum}!");
+        break;
+    }
 }
-if(gissningnum == rätttal){
+if (gissningnum == rätttal)
+{
     System.Console.WriteLine("Grattis du gissade rätt!");
     System.Console.WriteLine($"Talet är {rätttal}");
 }
-else if(gissningnum > rätttal)
+else if (gissningnum > rätttal)
 {
-Console.WriteLine("Du gissade för högt!");
-System.Console.WriteLine($"Talet är {rätttal}!");
+    Console.WriteLine("Du gissade för högt!");
+    System.Console.WriteLine($"Talet är {rätttal}!");
 }
-else if(gissningnum < rätttal){
-System.Console.WriteLine("Du gissade för lågt!");
-System.Console.WriteLine($"Talet är {rätttal}!");
+else if (gissningnum < rätttal)
+{
+    System.Console.WriteLine("Du gissade för lågt!");
+    System.Console.WriteLine($"Talet är {rätttal}!");
 }
 Console.ReadLine();
